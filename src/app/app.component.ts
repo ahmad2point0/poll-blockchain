09d46c0781bb35edac0a3d053825cc385f613f36
Muhammad Ahmad <aaziapk6@gmail.com>
2024-11-24 23:38:52 +0500

    add poll component and integrate into app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { PollCreateComponent } from "./poll-create/poll-create.component";
+import { PollCreateComponent } from './poll-create/poll-create.component';
 import { CommonModule } from '@angular/common';
+import { PollComponent } from './poll/poll.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PollCreateComponent,CommonModule],
+  imports: [RouterOutlet, PollCreateComponent, CommonModule, PollComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  showForm =false;
+  showForm = false;
+
+  polls = [
+    {
+    question: 'Do you like dogs or cats?',
+    votes: [0, 5, 4, 32, 3],
+    voted: true,
+    image:"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+    {
+    question: 'Do you like dogs or cats?',
+    votes: [0, 5, 4, 32, 3],
+    voted: false,
+    image:"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+    {
+    question: 'Do you like dogs or cats?',
+    votes: [0, 5, 4, 32, 3],
+    voted: true,
+    image:"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+    {
+    question: 'Do you like dogs or cats?',
+    votes: [0, 5, 4, 32, 3],
+    voted: false,
+    image:"https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  },
+];
 }
---
+++ src/app/poll/poll.component.ts
@@ -0,0 +1,25 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poll',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './poll.component.html',
+  styleUrl: './poll.component.scss',
+})
+export class PollComponent implements OnInit {
+  @Input() question!: string;
+  @Input() votes!: number[];
+  @Input() voted!: boolean;
+  @Input() pollImage!:string;
+  numberOfVotes!: number;
+  constructor() {}
+  ngOnInit(): void {
+    if (this.votes.length) {
+      this.numberOfVotes = this.votes.reduce((acc, curr) => {
+        return (acc += curr);
+      });
+    }
+  }
+}
